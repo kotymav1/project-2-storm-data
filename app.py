df59ace66cc7de6067dc81b1e7f8b0c6c079fd54
kotymav1 <koty@codacreations.com>
2024-06-02 21:28:41 -0600

    added file

--- app.py
+++ app.py
@@ -4,10 +4,14 @@ import pandas as pd
 app = Flask(__name__)
 
 # Load the cleaned data
-data = pd.read_csv('Resources/clean_data.csv')
+data = pd.read_csv('cleaned_data.csv')
 
 # Convert DataFrame to JSON
 data_json = data.to_dict(orient='records')
+
+@app.route('/', methods=['GET'])
+def home():
+    return "Welcome to the Storm Data API!"
 
 @app.route('/api/data', methods=['GET'])
 def get_data():
