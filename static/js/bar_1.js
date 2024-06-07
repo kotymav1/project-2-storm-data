938ea909ce7f5fd92befcbced20a7c900adb060b
nikimhenderson <nikimhenderson@gmail.com>
2024-06-06 18:50:14 -0600

    adding code so far to bar_1

---
+++ static/js/bar_1.js
@@ -0,0 +1,34 @@
+url = "http://127.0.0.1:5000/data"
+
+d3.json(url).then(function(data){
+    let properties = data.features.map(feature => feature.properties);
+    let xValues = properties.map(data => data.EVENT_TYPE);
+    let yValues = properties.map(data => data.DAMAGE_PROPERTY);
+    //console.log(xValues)
+    //console.log(yValues)
+    
+    let barTrace = [{
+        x: xValues,
+        y: yValues,
+        type: 'bar',
+        transforms: [{
+            type: 'aggregate',
+            groups: yValues,
+            aggregations: [
+                {target: 'y', func: 'sum', enabled: true},
+            ]
+        }]
+    }];
+    let layout = {
+        title: 'Damage by Event Type',
+        barmode: 'group',
+        xaxis: {
+            title: 'Event Type'
+        },
+        yaxis: {
+            title: 'Damage'
+        }
+    };
+    Plotly.newPlot("bar-1", barTrace, layout);
+
+});
